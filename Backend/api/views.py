b963b0562852b7652c19714b2efe93cfcaecad10
Radu Mihai Gheorghe <radu2002ro@gmail.com>
2023-08-05 00:04:16 +0300

    first commit

---
+++ Backend/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def getRoutes(request):
+    print(request)
+    test = {
+        "primulCamp": "primul camp",
+        "alDoileaCamp": "al doilea camp",
+        "alTreileaCamp": "al treilea camp",
+    }
+    return Response(test)
