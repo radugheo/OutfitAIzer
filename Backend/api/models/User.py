da6c362238a6e34ab2bb93b4fab1beff22e13b95
iulitaalpetri <iulitaalpetri66@gmail.com>
2023-08-08 20:11:04 +0300

    fixed: add EmailField and Id for User

--- Backend/api/models/User.py
+++ Backend/api/models/User.py
@@ -5,9 +5,10 @@ from django.db import models
 
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     ussername = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     picture = models.CharField()
     items = ArrayField(Item)
     outfits = ArrayField(Outfit)
