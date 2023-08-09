bb37579a128d67127cc30c9c068c1717e4c25590
iulitaalpetri <iulitaalpetri66@gmail.com>
2023-08-09 13:12:38 +0300

    feature: constant file

--- Backend/items/models/Item.py
+++ Backend/items/models/Item.py
@@ -1,10 +1,11 @@
+from constants import MAX_LENGTH
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class Item(models.Model):
-    type = models.CharField(max_length=100)
-    brand = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)
+    type = models.CharField(max_length=MAX_LENGTH)
+    brand = models.CharField(max_length=MAX_LENGTH)
+    color = models.CharField(max_length=MAX_LENGTH)
     image = models.CharField()
-    occasions = ArrayField(models.CharField(max_length=50))
+    occasions = ArrayField(models.CharField(max_length=MAX_LENGTH))
--- Backend/users/models/User.py
+++ Backend/users/models/User.py
@@ -1,3 +1,4 @@
+from constants import MAX_LENGTH
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from items.models.Item import Item
@@ -5,10 +6,10 @@ from outfits.models.Outfit import Outfit
 
 
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    id = models.AutoField(primary_key=MAX_LENGTH)
+    username = models.CharField(max_length=MAX_LENGTH)
+    password = models.CharField(max_length=MAX_LENGTH)
+    email = models.EmailField(max_length=MAX_LENGTH)
     picture = models.CharField()
     items = ArrayField(Item)
     outfits = ArrayField(Outfit)
