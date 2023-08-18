9d8875bd22eb07701bf089f2f9330854052172e4
iulitaalpetri <iulitaalpetri66@gmail.com>
2023-08-18 03:18:56 +0300

    error: Class Item has no objects member

--- Backend/item/urls.py
+++ Backend/item/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.postItem),
+    path("all", views.getAllItems),
 ]
--- Backend/item/views.py
+++ Backend/item/views.py
@@ -1,7 +1,17 @@
+import datetime
+
+import jwt
+from decouple import AutoConfig
+from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import Response
 
+from .models import Item
 from .serializers import ItemSerializer
+from .user.models import User
+
+config = AutoConfig()
 
 
 @api_view(["POST"])
@@ -9,4 +19,19 @@ def postItem(request):
     serializer = ItemSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def getAllItems(request):
+    token = request.COOKIES.get("jwt")
+    if not token:
+        raise AuthenticationFailed("Unauthenticated!")
+    try:
+        payload = jwt.decode(token, config("DJANGO_JWT_SECRET"), algorithms=["HS256"])
+    except jwt.ExpiredSignatureError:
+        raise AuthenticationFailed("Unauthenticated!")
+    user = User.objects.filter(id=payload["id"]).first()
+    items = Item.objects.filter(user=user)
+    serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
